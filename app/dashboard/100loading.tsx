6f4bb58dd16b13b92b3612818243686194d0dd95
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-03 13:40:37 -0500

    Optimization pga and Number page

---
+++ app/dashboard/100loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from "../ui/skeletons";
+
+export default function ssLoading(){
+  return (
+    <DashboardSkeleton></DashboardSkeleton>
+  );
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,8 +14,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -30,6 +30,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
