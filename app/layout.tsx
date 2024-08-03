ead84f2885157ec479a7fbf22bce85686472fd54
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-02 21:18:19 -0500

    Example project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+import "./ui/global.css";
+import { montserrat } from "./ui/fonts";
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${montserrat.className} antialiased`}>
+				Esto es parte del layout
+				{children}
+				<footer className="py-10 flex justify-center items-center"> Hecho con ❤️ por mi </footer>
+			</body>
+		</html>
+	);
 }
